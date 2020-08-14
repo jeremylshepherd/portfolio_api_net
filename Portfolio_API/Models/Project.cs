1000d64f91e56762dc34fb58977a5c6d272261f5
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2020-08-14 16:26:39 -0400

    Initial Commit.

---
+++ Portfolio_API/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Portfolio_API.Models
+{
+    public class Project
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Creator { get; set; }
+        public string User { get; set; }
+        public Uri Url { get; set; }
+        public string Type { get; set; }
+        public Uri Img { get; set; }
+        public string Description { get; set; }
+        public System.Collections.Generic.List<string> Technologies { get; set; }
+    }
+}
